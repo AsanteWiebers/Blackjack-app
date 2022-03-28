4073ae42df1f2f0e690c1debb0054b83e0327fff
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-03-28 23:05:09 +0200

    js basic functionality

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-
-let firstCard = 12;
+let firstCard = 8;
 let secondCard = 9;
 let blackjack = false;
 let isAlive = true;
 let message = " ";
 
+let messageEl = document.getElementById("welcome-elem");
+let sumEl = document.getElementById("sum-elem");
+
 let sum = firstCard + secondCard;
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if ( sum === 21){
-    message = "Whoo! You've got Blackjack!"
-    blackjack = true;
-} else {
-    message = "You're out of the game!";
-    isAlive = false;
+
+
+function startGame() {
+    
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if ( sum === 21){
+        message = "Whoo! You've got Blackjack!"
+        blackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum: " + sum;
 }
 
-console.log(message);
-console.log(blackjack);
-console.log(isAlive);
+
+
+startGame();
+
+
+// console.log(message);
+// console.log(blackjack);
+// console.log(isAlive);
 
